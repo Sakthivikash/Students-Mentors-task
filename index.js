65ade12f2b0b12f0d0b1c66867c010883c26c142
Sakthivikash <sakthivikash70@gmail.com>
2022-04-29 18:08:47 +0530

    Welcome note

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -15,10 +15,10 @@ app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL;
 
 async function createConnection() {
-  const client = new MongoClient(MONGO_URL);
-  await client.connect();
-  console.log("Mongo is connected ✌️😊");
-  return client;
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log("Mongo is connected ✌️😊");
+    return client;
 }
 export const database = await createConnection();
 
@@ -27,9 +27,13 @@ var date_ob = new Date();
 var dateTime = `${date_ob.getFullYear()}-${("0" + (date_ob.getMonth() + 1)).slice(-2)}-${("0" + date_ob.getDate()).slice(-2)} T${date_ob.getHours()}:${date_ob.getMinutes()}:${date_ob.getSeconds()}`;
 console.log(dateTime);
 
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Webdeveloper task (Mentor and Student Assigning with Database)</h1>')
+})
+
 app.use('/students', studentsRouter);
 app.use('/mentors', mentorsRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 });
